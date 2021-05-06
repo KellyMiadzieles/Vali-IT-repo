a3512ec511382918399586b7fe8b68102016e43b
Kelly Miadzieles <kellymiadzieles@gmail.com>
2021-05-06 14:11:10 +0300

    commit

---
+++ src/main/java/ee/bcs/valiit/scheduleProov/Schedule_test.java
@@ -0,0 +1,15 @@
+package ee.bcs.valiit.scheduleProov;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule_test {
+
+//    @Scheduled (fixedDelay = 1000) // üks sekund
+//    // (cron = 30 30 * * * MON-FRI) läheb tööle 13:30:30, 14:30:30
+//    // (cron = 0 0 2 * * * MON-FRI) läheb tööle südaöösel kell kaks esmasp-reede
+//    public void test(){
+//        System.out.println("läks tööle");
+//    }
+}
+
